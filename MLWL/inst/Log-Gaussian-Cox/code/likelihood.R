f1c3550a5e98e81281f709ceb826239a92f9f011
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-01 18:55:55 -0400

    Update likelihood.R

--- MLWL/inst/Log-Gaussian-Cox/code/likelihood.R
+++ MLWL/inst/Log-Gaussian-Cox/code/likelihood.R
@@ -1,3 +1,7 @@
 ### likelihood
 loglikelihood <- function(x) return(coxprocess_loglikelihood(matrix(x, nrow = 1), data_counts, parameter_area))
 gradloglikelihood <- function(x) return(matrix(data_counts, nrow = 1) - parameter_area * exp(matrix(x, nrow = 1)))
+
+loglikelihood_matrix <- function(x) return(coxprocess_loglikelihood(x, data_counts, parameter_area))
+gradloglikelihood_matrix <- function(x) return(data_counts - parameter_area * exp(x))
+
