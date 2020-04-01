dd1d7b0f7ae0b5a50c9958e720fa5f7c9d8bd797
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-01 18:47:12 -0400

    Update configuration.R

--- MLWL/inst/Log-Gaussian-Cox/SteppingStone/configuration.R
+++ MLWL/inst/Log-Gaussian-Cox/SteppingStone/configuration.R
@@ -14,3 +14,13 @@ source("code/likelihood.R")
 
 ### HMC
 source("code/HMC.R")
+
+### load the temeprature sequence
+load(paste("sequence/", ngrid, ".RData", sep = ""))
+temperature <- data_save$sequence
+
+### load the mode
+load(paste("mode/", ngrid, ".RData", sep = ""))
+x0 <- data_save$surrogate_mu
+
+setwd("./SteppingStone")
