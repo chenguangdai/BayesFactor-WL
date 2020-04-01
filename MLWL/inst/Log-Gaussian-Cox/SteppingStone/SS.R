be1c89cd5eafbd65a8d83685ba5ad3e49e430b31
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-01 18:40:33 -0400

    Create SS.R

---
+++ MLWL/inst/Log-Gaussian-Cox/SteppingStone/SS.R
@@ -0,0 +1,58 @@
+### The stepping stone method for the Log-Gaussian Cox process
+rm(list = ls())
+library(debiasedhmc)
+library(spatstat)
+
+### square size
+ngrid <- 20
+### configuration
+source("/Users/chenguang/Desktop/working papers/MTM/revision/code/log-cox-process/Stepping Stone/configuration.R")
+### load the mode
+load(paste("/Users/chenguang/Desktop/working papers/MTM/revision/code/log-cox-process/Stepping Stone/mode/mode", ngrid, ".RData", sep = ""))
+x0 <- data_save$surrogate_mu
+### load the temeprature sequence
+load(paste("/Users/chenguang/Desktop/working papers/MTM/revision/code/log-cox-process/Stepping Stone/sequence/", ngrid, ".RData", sep = ""))
+temperature <- data_save$sequence
+nparticles <- 1.5*10^3
+
+### Stepping stone
+start_time <- Sys.time()
+lognormconst_ratio <- rep(NA, length(temperature) - 1)
+for(iter in 1:(length(temperature) - 1)){
+  ### temperature
+  gamma_target <- temperature[iter + 1]
+  gamma_proposal <- temperature[iter]
+  
+  ### generate samples
+  if(iter == 1){
+    xparticles <- prior$rinit(n = nparticles)
+  }else{
+    ### define the proposal used in importance sampling
+    proposal <- list()
+    proposal$logdensity <- function(x) return(prior$logdensity(x) + gamma_proposal*loglikelihood(x))
+    proposal$gradlogdensity <- function(x) return(prior$gradlogdensity(x) + gamma_proposal*gradloglikelihood(x))
+    
+    ### define the proposal kernel
+    proposal_kernel <- construct_kernel(proposal)
+    
+    ### generate samples using MCMC
+    xparticles <- matrix(0, nrow = nparticles, ncol = dimension)
+    xparticles[1, ] <- x0
+    for(i in 2:nparticles){xparticles[i, ] <- proposal_kernel(matrix(xparticles[i - 1, ], nrow = 1))}
+  }
+  
+  ### calculate the log normalizing constant ratio
+  logw <- (gamma_target - gamma_proposal) * loglikelihood(xparticles)
+  maxlw <- max(logw)
+  lognormconst_ratio[iter] <- log(mean(exp(logw - maxlw))) + maxlw
+  
+  print(iter/length(temperature))
+}
+
+end_time <- Sys.time()
+
+
+lognormconst <- sum(lognormconst_ratio)
+running_time <- end_time - start_time
+
+
