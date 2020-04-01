f5f52e694ce1f161422a3c721e3f1656f92dd0e2
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-03-31 22:04:19 -0400

    Create load_data.R

---
+++ MLWL/inst/Log-Gaussian-Cox/code/load_data.R
@@ -0,0 +1,18 @@
+### load in data and set up parameters
+data(finpines)
+data_x <- (finpines$x + 5) / 10
+data_y <- (finpines$y + 8) / 10
+grid <- seq(from = 0, to = 1, length.out = ngrid + 1)
+dimension <- ngrid^2
+data_counts <- rep(0, dimension)
+for (i in 1:ngrid){
+  for (j in 1:ngrid){
+    logical_y <- (data_x > grid[i]) * (data_x < grid[i+1])
+    logical_x <- (data_y > grid[j]) * (data_y < grid[j+1])
+    data_counts[(i-1)*ngrid + j] <- sum(logical_y * logical_x)
+  }
+}
+parameter_sigmasq <- 1.91
+parameter_beta <- 1/33
+parameter_area <- 1 / dimension
+parameter_mu <- log(126) - 0.5 * parameter_sigmasq
