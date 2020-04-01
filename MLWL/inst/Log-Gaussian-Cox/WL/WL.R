16c26291d5b46427b262b1ba0a964b6bb0c319c4
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-03-31 21:51:28 -0400

    Update WL.R

--- MLWL/inst/Log-Gaussian-Cox/WL/WL.R
+++ MLWL/inst/Log-Gaussian-Cox/WL/WL.R
@@ -17,11 +17,11 @@ x0 <- surrogate_mu
 
 ##### run the WL mixture algorithm
 start_time <- Sys.time()
-log_marginal_likelihood <- MLWL(num_iterations, num_burnin, target_kernel, surrogate_kernel,
-                                target_logdensity, surrogate_logdensity, learning_rate, flatness_criterion, x0)
+result <- MLWL(num_iterations, num_burnin, target_kernel, surrogate_kernel,
+               target_logdensity, surrogate_logdensity, learning_rate, flatness_criterion, x0)
 end_time <- Sys.time()
 running_time <- end_time - start_time
-data_save <- list(lognormconst = log_marginal_likelihood, running_time = running_time)
+data_save <- list(lognormconst = result$log_marginal_likelihood, running_time = running_time)
 
 
 
