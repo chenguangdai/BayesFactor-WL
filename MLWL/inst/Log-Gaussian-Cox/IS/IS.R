f9175149b215b7eaae15f72e5fcedfe9500159bf
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-01 20:16:03 -0400

    Update IS.R

--- MLWL/inst/Log-Gaussian-Cox/IS/IS.R
+++ MLWL/inst/Log-Gaussian-Cox/IS/IS.R
@@ -1,1 +1,24 @@
+### Importance sampling for the Log-Gaussian Cox process
+rm(list = ls())
+library(MASS)
+library(debiasedhmc)
+library(spatstat)
+
+### define the number of grids
+ngrid <- 10
+
+### configuration
+source("configuration.R")
+
+### importance sampling
+start_time <- Sys.time()
+num_samples <- 10^6
+xparticles <- surrogate$rinit(num_samples)
+logw <- rep(0, num_samples)
+logw <- target$logdensity(xparticles) - surrogate$logdensity(xparticles)
+maxlw <- max(logw)
+lognormconst <- log(mean(exp(logw - maxlw))) + maxlw
+end_time <- Sys.time()
+running_time <- end_time - start_time
+
 
