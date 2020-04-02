2e45a1d719482a2777bc522497339a6b65a5b41c
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-02 18:22:51 -0400

    Update load_data.R

--- MLWL/inst/Logistic/load_data.R
+++ MLWL/inst/Logistic/load_data.R
@@ -1,2 +1,21 @@
 ### load in data
-data <- read.table(paste(getwd(), "/data/germancredit.txt", sep = ""), header = F, sep = '')
+data <- read.table("data/germancredit.txt", header = F)
+X <- data[,1:24]
+X <- scale(X)
+Y <- data[,25]
+Y <- Y - 1
+n <- nrow(X)
+p <- ncol(X)
+
+Xinter <- matrix(nrow = n, ncol = p*(p-1) / 2)
+index <- 1
+for (j in 1:(p-1)){
+  for (jprime in (j+1):p){
+    Xinter[,index] <- X[,j] * X[,jprime]
+    index <- index + 1
+  }
+}
+Xinter <- scale(Xinter)
+X <- cbind(X, Xinter)
+p <- ncol(X)
+dimension <- p + 2
