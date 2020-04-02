7079772579ae011cd087b49da7faaf6ecce9191d
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-02 16:45:03 -0400

    Update configuration.R

--- MLWL/inst/Bayesian-Lasso/WL/configuration.R
+++ MLWL/inst/Bayesian-Lasso/WL/configuration.R
@@ -1,4 +1,6 @@
-##### pre-calculate quantity
+setwd('..')
+
+### pre-calculate quantity
 XtX <- t(X)%*%X
 Xty <- c(t(X)%*%y)
 yty <- sum(y^2)
@@ -6,8 +8,9 @@ n <- length(y)
 p <- ncol(X)
 dimension <- 2*p + 1
 
-##### The target distribution
-target_logdensity <- function(x){
+### the target distribution
+target <- list()
+target$logdensity <- function(x){
   beta <- x[1:p]
   eta <- x[(p + 1):(2*p)]
   xi <- x[2*p + 1]
@@ -16,17 +19,17 @@ target_logdensity <- function(x){
   return(part1 + part2)
 }
 
-##### The surrogate distribution
-load(paste(getwd(), "/data/VI/VI_SNR_", SNR, "_lambda_", lambda, ".RData", sep = ""))
-# load(paste(getwd(), "/data/mode/mode_SNR_", SNR, "_lambda_", lambda, ".RData", sep = ""))
+### the surrogate distribution
+surrogate <- list()
+load("data/VI/VI_SNR_", SNR, "_lambda_", lambda, ".RData", sep = ""))
 surrogate_mu <- c(data_save$surrogate_m, data_save$surrogate_phi, data_save$surrogate_u)
 surrogate_sigma <- sqrt(c(data_save$surrogate_ssq, data_save$surrogate_zetasq, data_save$surrogate_vsq))
-surrogate_logdensity <- function(x){
+surrogate$logdensity <- function(x){
   return(-dimension/2*log(2*pi) - sum(log(surrogate_sigma)) - 0.5 * sum((x - surrogate_mu)^2/surrogate_sigma^2))
 } 
 
-##### A Gibbs kernel invariant to the target distribution
-target_kernel <- function(x){
+### a Gibbs kernel invariant to the target distribution
+target$kernel <- function(x){
   beta <- x[1:p]
   tausq <- exp(x[(p + 1):(2*p)])
   sigmasq <- exp(x[2*p + 1])
@@ -40,8 +43,8 @@ target_kernel <- function(x){
   return(c(beta, log(tausq), log(sigmasq)))
 }
 
-##### Direct sampling from the surrogate distribution
-surrogate_kernel <- function(x){
+### direct sampling from the surrogate distribution
+surrogate$kernel <- function(x){
   return(rnorm(dimension, mean = surrogate_mu, sd = surrogate_sigma))
 }
 
