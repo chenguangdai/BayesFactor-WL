97d4e38eb13f9fccdb658ca5bc8c1eb112629e28
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-01 22:39:53 -0400

    Update SMC.R

--- MLWL/inst/Log-Gaussian-Cox/SMC/SMC.R
+++ MLWL/inst/Log-Gaussian-Cox/SMC/SMC.R
@@ -1,6 +1,26 @@
+### SMC for the Log-Gaussian Cox process
+rm(list = ls())
 library(MASS)
-library(Rcpp)
-library(RcppEigen)
+library(debiasedhmc)
 library(spatstat)
 library(smcUtils)
 library(mgcv)
+
+### define the number of grids
+ngrid <- 10
+
+### configuration
+source("configuration.R")
+
+### SMC configuration
+start_time <- Sys.time()
+mcmc_kernel <- list()
+mcmc_kernel$parameters <- list(stepsize = 0.25, nsteps = 10)
+mcmc_kernel$nmcmc <- 10
+nparticles <- 500
+cess_criterion <- 0.9
+smc_output <- run_smc(target, proposal, nparticles, mcmc_kernel = mcmc_kernel, cess_criterion)
+end_time <- Sys.time()
+running_time <- end_time - start_time
+data_save <- list(lognormconst = smc_output$log_ratio_normconst, running_time = running_time)
+
