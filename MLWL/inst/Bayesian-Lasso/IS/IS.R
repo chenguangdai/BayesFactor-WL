aa36253fdc2b5fcf577b59c2bf53a1127087e27d
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-02 18:01:05 -0400

    Create IS.R

---
+++ MLWL/inst/Bayesian-Lasso/IS/IS.R
@@ -0,0 +1,20 @@
+### importance sampling for Bayesian Lasso
+rm(list = ls())
+library(debiasedhmc)
+
+### source C functions
+sourceCpp("mvnorm.cpp")
+
+### SNR and lambda
+SNR <- 3
+lambda <- 5
+
+### importance sampling
+start_time <- Sys.time()
+num_samples <- 5*10^5
+xparticles <- proposal$rinit(num_samples)
+logw <- target$logdensity(xparticles) - proposal$logdensity(xparticles)
+maxlw <- max(logw)
+lognormconst <- log(mean(exp(logw - maxlw))) + maxlw
+end_time <- Sys.time()
+running_time <- end_time - start_time
