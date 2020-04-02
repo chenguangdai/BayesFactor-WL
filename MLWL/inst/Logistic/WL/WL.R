d1f31dc93d4835485d609afb68f8724547301d9f
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-02 18:33:29 -0400

    Update WL.R

--- MLWL/inst/Logistic/WL/WL.R
+++ MLWL/inst/Logistic/WL/WL.R
@@ -1,29 +1,25 @@
-##### Normalizing constant estimation for Logistic regression
+### the WL mixture method for Logistic regression
 rm(list = ls())
 library(MLWL)
 library(debiasedhmc)
 
-##### set a correct path
-setwd(paste(getwd(), '/inst/Logistic', sep = ""))
-
-##### load in data
-data <- read.table(paste(getwd(), "/data/germancredit.txt", sep = ""), header = F, sep = '')
+### lambda
 lambda <- 0.01
 
-##### configuration
+### configuration
 source("configuration.R")
+
 num_iterations <- 5000
 num_burnin <- num_iterations/2
 learning_rate <- 1
 flatness_criterion <- 0.2
 x0 <- surrogate_mean
 
-##### run the Wang-Landau algorithm
+### the WL mixture method
 start_time <- Sys.time()
-log_marginal_likelihood <- MLWL(num_iterations, num_burnin, target_kernel, surrogate_kernel,
-                                target_logdensity, surrogate_logdensity, learning_rate, flatness_criterion, x0)
+result <- MLWL(num_iterations, num_burnin, target$kernel, surrogate$kernel, target$logdensity, 
+               surrogate$logdensity, learning_rate, flatness_criterion, x0, suppress = F)
+lognormconst <- result$log_marginal_likelihood
 end_time <- Sys.time()
 running_time <- end_time - start_time
-print(paste("The log marginal likelihood is", log_marginal_likelihood))
-print(paste("The running time is", running_time))
 
