ba6cec83cbd0eec502987abf17645229d14a8d50
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-02 18:44:41 -0400

    Update BS.R

--- MLWL/inst/Logistic/BS/BS.R
+++ MLWL/inst/Logistic/BS/BS.R
@@ -1,8 +1,9 @@
+### bridge sampling for Logistic regression
 rm(list = ls())
 library(rstan)
 library(bridgesampling)
 
-##### stan model
+### stan model
 LogisticRegression.stan <- '
 data {
   int<lower = 0> n;       // number of observations
@@ -32,50 +33,26 @@ model {
 }
 '
 
-##### data preparation 
-germancredit <- read.table("/Users/chenguang/Desktop/working papers/MTM/revision/code/Logistic/data/germancredit.txt", header = F, sep = '')
-X <- germancredit[, 1:24]
-X <- scale(X)
-y <- germancredit[, 25]
-y <- y - 1
-n <- nrow(X)
-p <- ncol(X)
-Xinter <- matrix(nrow = n, ncol = p*(p-1) / 2)
-index <- 1
-for (j in 1:(p-1)){
-  for (jprime in (j+1):p){
-    Xinter[,index] <- X[,j] * X[,jprime]
-    index <- index + 1
-  }
-}
-Xinter <- scale(Xinter)
-X <- cbind(X, Xinter)
-p <- ncol(X)
+### load in data
+setwd('..')
+source("load_data.R")
+setwd('./BS')
+
+### lambda
 lambda <- 1
 
-##### run Stan 
-num_replicate <- 10
-for(replicate in 1:num_replicate){
-  warmup <- 1000
-  iteration <- 3500
-  stan.fit <- stan(model_code = LogisticRegression.stan, 
-                   data = list(
-                     n = n,
-                     p = p,
-                     lambda = lambda,
-                     X = X,
-                     y = y),
-                   warmup = warmup, iter = iteration, chains = 1, control = list(adapt_delta = 0.8))
-  stan.fit.extract = extract(stan.fit)
-  bridge_result <- bridge_sampler(stan.fit, method = "normal", maxiter = 1000)
-  print(replicate)
-  data_save <- list(lognormconst = bridge_result$logml)
-  save(data_save, file = paste("/Users/chenguang/Desktop/working papers/MTM/revision/code/Logistic/result/BS/", 
-                               iteration - warmup, "/1.00", "/replicate_", 
-                               replicate, ".RData", sep = ""))
-}
-
-
-
-
-
+### run stan
+warmup <- 1000
+iteration <- 3500
+stan.fit <- stan(model_code = LogisticRegression.stan, 
+                 data = list(
+                   n = n,
+                   p = p,
+                   lambda = lambda,
+                   X = X,
+                   y = y),
+                 warmup = warmup, iter = iteration, chains = 1, control = list(adapt_delta = 0.8))
+stan.fit.extract = extract(stan.fit)
+bridge_result <- bridge_sampler(stan.fit, method = "normal", maxiter = 1000)
+lognormconst = bridge_result$logml)
+ 
