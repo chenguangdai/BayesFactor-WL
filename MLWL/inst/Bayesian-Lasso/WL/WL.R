6f5180a7d3917a784eab45502e5eb5e79b729655
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-02 17:05:10 -0400

    Update WL.R

--- MLWL/inst/Bayesian-Lasso/WL/WL.R
+++ MLWL/inst/Bayesian-Lasso/WL/WL.R
@@ -1,1 +1,26 @@
+### WL mixture method for Bayesian Lasso
+rm(list = ls())
+library(MLWL)
+library(geoR)
+library(statmod)
+library(mvtnorm)
 
+### SNR and lambda
+SNR <- 3
+lambda <- 5
+
+### configuration
+source("configuration.R")
+num_iterations <- 10^4
+num_burnin <- 2000
+learning_rate <- 1
+flatness_criterion <- 0.2
+x0 <- surrogate_mu
+
+### the WL mixture method
+start_time <- Sys.time()
+result <- MLWL(num_iterations, num_burnin, target$kernel, surrogate$kernel, target$logdensity, 
+               surrogate$logdensity, learning_rate, flatness_criterion, x0, suppress = F)
+end_time <- Sys.time()
+lognormconst <- result$log_marginal_likelihood
+running_time <- end_time - start_time
