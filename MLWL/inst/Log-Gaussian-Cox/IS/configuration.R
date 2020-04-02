f290b194ce3353f9359ade2526e39c6c328f668e
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-01 20:13:15 -0400

    Update configuration.R

--- MLWL/inst/Log-Gaussian-Cox/IS/configuration.R
+++ MLWL/inst/Log-Gaussian-Cox/IS/configuration.R
@@ -1,64 +1,40 @@
-# parameters
-data(finpines)
-data_x <- (finpines$x + 5) / 10 # normalize data to unit square
-data_y <- (finpines$y + 8) / 10
-grid <- seq(from = 0, to = 1, length.out = ngrid + 1)
-dimension <- ngrid^2
-data_counts <- rep(0, dimension)
-for (i in 1:ngrid){
-  for (j in 1:ngrid){
-    logical_y <- (data_x > grid[i]) * (data_x < grid[i+1])
-    logical_x <- (data_y > grid[j]) * (data_y < grid[j+1])
-    data_counts[(i-1)*ngrid + j] <- sum(logical_y * logical_x)
-  }
-}
-parameter_sigmasq <- 1.91
-parameter_beta <- 1/33
-parameter_area <- 1 / dimension
-parameter_mu <- log(126) - 0.5 * parameter_sigmasq
-prior_mean <- rep(parameter_mu, dimension)
-prior_cov <- matrix(nrow = dimension, ncol = dimension)
-for(m in 1:dimension){
-  for(n in 1:dimension){
-    index_m <- c(floor((m - 1)/ngrid) + 1, ((m - 1)%%ngrid) + 1)
-    index_n <- c(floor((n - 1)/ngrid) + 1, ((n - 1)%%ngrid) + 1)
-    prior_cov[m, n] <- parameter_sigmasq * exp(-sqrt(sum((index_m - index_n)^2))/(ngrid * parameter_beta))
-  }
-}
-prior_precision <- solve(prior_cov)
-prior_precision_chol <- t(chol(prior_precision))
-prior <- list()
-prior$logdensity <- function(x) return(dmvnorm_cholesky_inverse(x, prior_mean, prior_precision_chol))
+setwd('..')
 
-# target
-likelihood <- list()
-likelihood$log <- function(x) return(coxprocess_loglikelihood(x, data_counts, parameter_area))
-targetlogdensity <- function(x) return(prior$logdensity(x) + likelihood$log(x))
+### load in C functions
+sourceCpp("code/mvnorm.cpp", verbose = F)
 
+### load in data and set up parameters
+source("code/load_data.R")
 
-##### surrogate distribution
-# load(paste("/Users/chenguang/Desktop/working papers/MTM/revision/code/log-cox-process/WL/surrogate_moments/ngrid_", 
-#            ngrid, ".RData", sep = ""))
-load(paste("/Users/chenguang/Desktop/working papers/MTM/revision/code/log-cox-process/IS/mode/mode", 
-           ngrid, ".RData", sep = ""))
-surrogate_mean <- data_save$surrogate_mu
-surrogate_cov <- matrix(0, nrow = dimension, ncol = dimension)
-# diag(surrogate_cov) <- mean(diag(surrogate_moments$surrogate_cov))
+### define the target distribution
+### prior distribution
+source("code/prior.R")
+
+### likelihood
+source("code/likelihood.R")
+
+### target
+target <- list()
+target$logdensity <- function(x) return(prior$logdensity(x) + loglikelihood_matrix(x))
+
+### define the surrogate distribution
+load(paste("mode/", ngrid, ".RData", sep = ""))
+surrogate_mu <- data_save$surrogate_mu
 if(ngrid == 10){
-  diag(surrogate_cov) <- 1.0^2
+  surrogate_sigma <- rep(1.0, dimension)
 }else if(ngrid == 20){
-  diag(surrogate_cov) <- 1.2^2
+  surrogate_sigma <- rep(1.2, dimension)
 }else{
-  diag(surrogate_cov) <- 1.3^2
+  surrogate_sigma <- rep(1.3, dimension)
 }
-surrogate_precision <- solve(surrogate_cov)
-surrogate_precision_chol <- t(chol(surrogate_precision))
-surrogatelogdensity <- function(x) return(dmvnorm_cholesky_inverse(x, surrogate_mean, surrogate_precision_chol))
-surrogaterinit <- function(n){
+surrogate <- list()
+surrogate$logdensity <- function(x){
+  return(-dimension/2*log(2*pi) - sum(log(surrogate_sigma)) - 0.5 * sum((x - surrogate_mu)^2/surrogate_sigma^2))
+} 
+surrogate$rinit <- function(n){
   xparticles <- matrix(0, nrow = n, ncol = dimension)
-  sigma <- sqrt(diag(surrogate_cov))
   for(j in 1:dimension){
-    xparticles[, j] <- rnorm(n, mean = surrogate_mean[j], sd = sigma[j])
+    xparticles[, j] <- rnorm(n, mean = surrogate_mu[j], sd = surrogate_sigma)
   }
   return(xparticles)
 } 
