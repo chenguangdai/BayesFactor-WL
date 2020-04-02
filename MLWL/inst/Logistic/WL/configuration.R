71a58b13b07980fdb7358949d64f6ac464d20ffd
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-02 18:28:50 -0400

    Update configuration.R

--- MLWL/inst/Logistic/WL/configuration.R
+++ MLWL/inst/Logistic/WL/configuration.R
@@ -1,38 +1,24 @@
-##### data
-X <- data[,1:24]
-X <- scale(X)
-Y <- data[,25]
-Y <- Y - 1
-n <- nrow(X)
-p <- ncol(X)
+setwd('..')
 
-Xinter <- matrix(nrow = n, ncol = p*(p-1) / 2)
-index <- 1
-for (j in 1:(p-1)){
-  for (jprime in (j+1):p){
-    Xinter[,index] <- X[,j] * X[,jprime]
-    index <- index + 1
-  }
-}
-Xinter <- scale(Xinter)
-X <- cbind(X, Xinter)
-p <- ncol(X)
-dimension <- p + 2
+### load in data
+source("load_data.R")
 
-##### The target distribution
-target_logdensity <- function(x) debiasedhmc:::logistic_logtarget_c(x, Y, X, lambda)
-target_gradlogdensity <- function(x) debiasedhmc:::logistic_gradlogtarget_c(x, Y, X, lambda)
+### the target distribution
+target <- list()
+target$logdensity <- function(x) debiasedhmc:::logistic_logtarget_c(x, Y, X, lambda)
+target$gradlogdensity <- function(x) debiasedhmc:::logistic_gradlogtarget_c(x, Y, X, lambda)
 
-##### The surrogate distribution
-load(paste(getwd(), "/data/posterior_moments_lambda_", lambda, ".RData", sep = ""))
+### the surrogate distribution
+surrogate <- list()
+load("data/posterior_moments_lambda_", lambda, ".RData")
 surrogate_mean <- data_save$mean
 surrogate_cov <- data_save$cov
 surrogate_precision <- solve(surrogate_cov)
 surrogate_precision_chol <- t(chol(surrogate_precision))
-surrogate_logdensity <- function(x) return(fast_dmvnorm_chol_inverse(matrix(x, nrow = 1), surrogate_mean, surrogate_precision_chol))
-surrogate_rinit <- function() return(fast_rmvnorm(n = 1, mean = surrogate_mean, covariance = surrogate_cov))
+surrogate$logdensity <- function(x) return(fast_dmvnorm_chol_inverse(matrix(x, nrow = 1), surrogate_mean, surrogate_precision_chol))
+surrogate$rinit <- function() return(fast_rmvnorm(n = 1, mean = surrogate_mean, covariance = surrogate_cov))
 
-##### A HMC kernel invariant to the target distribution
+### HMC
 stepsize <- 0.03
 nsteps <- 10
 ### leapfrog
@@ -47,8 +33,8 @@ leapfrog <- function(x, v, gradient){
   v <- v + stepsize * gradient(x) / 2
   return(list(x = x, v = v))
 }
-### HMC
-target_kernel <- function(x){
+
+target$kernel <- function(x){
   x_current <- x
   v_current <- matrix(rnorm(dimension), nrow = 1)
   ### leapfrog steps
@@ -63,5 +49,7 @@ target_kernel <- function(x){
   return(x)
 }
 
-### Directly sampling from the surrogate distribution
-surrogate_kernel <- function(x) return(surrogate_rinit())
+### directly sampling from the surrogate distribution
+surrogate$kernel <- function(x) return(surrogate_rinit())
+
+setwd('./WL')
