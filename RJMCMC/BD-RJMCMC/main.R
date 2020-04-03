a26d82fe2e0237e5f841445ad46ca065f50ca749
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-03 10:34:22 -0400

    Update main.R

--- RJMCMC/BD-RJMCMC/main.R
+++ RJMCMC/BD-RJMCMC/main.R
@@ -1,1 +1,45 @@
+### MTM-RJMCMC on pollution data
+rm(list = ls())
+library(mvnfast)
+library(LaplacesDemon)
+library(pscl)
+
+### sparsity
+g <- exp(10)
+
+### the standard deviation of the proposal distribution for beta
+sigma_proposal <- 0.5
+
+
+### configuration
+source("configuration.R")
+
+### MCMC
+num_iter <- 1.5*10^5
+beta <- matrix(NA, nrow = num_iter, ncol = p)
+sigma <- rep(1, num_iter)
+marginal_prob <- rep(0, p)
+
+### random initialization
+initial_variable_index <- sample(x = 1:p, size = p, replace = F)
+beta[1, initial_variable_index] <- lm(Y ~ as.matrix(X[, initial_variable_index], nrow = n) - 1)$coef
+sigma[1] <- update_sigma(beta[1, ])
+
+start_time <- Sys.time()
+for(iter in 2:num_iter){
+  ### condition on sigma, update beta
+  beta[iter, ] <- BD_RJMCMC(beta[iter - 1, ], sigma[iter - 1])
+  
+  ### condition on beta, update sigma
+  sigma[iter] <- update_sigma(beta[iter, ])
+  
+  ### burn in the first 10%
+  if(iter > 0.1*num_iter){
+    selected_variable_index <- which(!is.na(beta[iter, ]))
+    marginal_prob[selected_variable_index] <- marginal_prob[selected_variable_index] + 1
+  }
+}
+end_time <- Sys.time()
+marginal_prob <- marginal_prob/sum(marginal_prob)
+running_time <- as.numeric(end_time - start_time)
 
