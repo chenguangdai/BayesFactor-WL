aa589166200d543d16e2ec8e51b1a6bdf0368207
chenguangdai <40900789+chenguangdai@users.noreply.github.com>
2020-04-02 23:01:15 -0400

    Update gibbs.R

--- RJMCMC/MTM-RJMCMC/gibbs.R
+++ RJMCMC/MTM-RJMCMC/gibbs.R
@@ -1,16 +1,21 @@
-gibbs <- function(beta, sigma){
-  ### conditioning on sigma, update beta
+### conditioning on sigma, update beta
+update_beta <- function(beta, sigma){
   index <- which(!is.na(beta))
   Xgamma <- as.matrix(X[, index], nrow = n)
   XtXinv <- solve(t(Xgamma)%*%Xgamma)
   XtY <- t(Xgamma)%*%Y
   beta[index] <- rmvn(n = 1, mu = g/(g + 1)*XtXinv%*%XtY, sigma = g/(g + 1)*sigma^2*XtXinv)
-  
-  ### conditioning on beta, update sigma
+  return(beta)
+}
+
+### conditioning on beta, update sigma
+update_sigma <- function(beta){
+  index <- which(!is.na(beta))
   fittedvalue <- as.matrix(X[, index], nrow = n)%*%beta[index]
   a <- (n + length(index))/2
   b <- 0.5*((1/g)*sum(fittedvalue^2) + sum((Y - fittedvalue)^2))
   sigma <- sqrt(rigamma(1, alpha = a, beta = b))
-  
-  return(list(beta = beta, sigma = sigma))
+  return(sigma)
+}
+ist(beta = beta, sigma = sigma))
 }
